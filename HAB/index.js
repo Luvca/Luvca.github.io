6fdbf24ae184cb4a719033f00861e7e923d3292e
Luvca <committer@luvca.local>
2018-04-26 18:45:01 +0900

    Update js

--- HAB/index.js
+++ HAB/index.js
@@ -1,6 +1,26 @@
 (function() {
   Office.initialize = function(reason) {
     $document.ready(function() {
+      $("#getGroupsButton").click(function() {
+        Office.context.mailbox.getCallbackTokenAsync({isRest: true}, function(result) {
+          if (result.status == Office.AsyncResultStatus.Succeeded) {
+            var accessToken = result.value;
+            var host = Office.context.mailbox.restUrl;
+            var url = host + "/beta/groups";
+
+            $ajax({
+              type: "GET",
+              url: url,
+              dataType: "json",
+              headers: {"Authorization": "Bearer " + accessToken}
+            }).done(function(response, textStatus, jqXHR) {
+               $("#result").val(JSON.stringify(response));
+            }).fail(function(jqXHR, textStatus, errorThrown) {
+               $("#result").val("Error: " + errorThrown);
+            });
+          }
+        });
+      });
     });
   };
 })();
