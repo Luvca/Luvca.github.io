9cca7cedee19d31307b7cd03791a411c80631ada
Luvca <committer@luvca.local>
2018-04-27 19:29:12 +0900

    Update js, html

--- HAB/index.js
+++ HAB/index.js
@@ -1,8 +1,29 @@
+//var graphApiEndpoint = "https://graph.microsoft.com/beta/groups";
+var graphApiEndpoint = "https://graph.microsoft.com/v1.0/me";
+var graphApiScopes = ["https://graph.microsoft.com/user.read"];
+var userAgentApplication = new Msal.UserAgentApplication(msalconfig.clientID, null, loginCallback, {
+  redirectUri: msalconfig.redirectUri
+});
+if (userAgentApplication.redirectUri) {
+  userAgentApplication.redirectUri = msalconfig.redirectUri;
+}
+
+var msalconfig = {
+  clientID: "",
+  redirectUri: location.origin
+};
+
 (function() {
   Office.initialize = function(reason) {
     $(document).ready(function() {
+      if (!userAgentApplication.isCallback(window.location.hash) && window.parent === window && !window.opener) {
+        var user = userAgentApplication.getUser();
+        if (user) {
+          callGraphApi();
+        }
+      }
+      /*
       $("#getGroupsButton").click(function() {
-        $("#log").val("click");
         Office.context.mailbox.getCallbackTokenAsync({isRest: true}, function(result) {
           if (result.status == Office.AsyncResultStatus.Succeeded) {
             var accessToken = result.value;
@@ -26,6 +47,91 @@
           }
         });
       });
+      */
     });
   };
 })();
+
+function callGraphApi() {
+  var user = userAgentApplication.getUser();
+  if (!user) {
+    userAgentApplication.loginRedirect(graphApiScopes);
+  } else {
+    var userInfoElement = $("#userInfo");
+    userInfoElement.parentElement.classList.remove("hidden");
+    userInfoElement.innerHTML = JSON.stringify(user, null, 4);
+    $("#signOutButton").classList.remove("hidden");
+    var graphCallResponseElement = $("#graphResponse");
+    graphCallResponseElement.parentElement.classList.remove("hidden");
+    graphCallResponseElement.innerText = "Calling graph...";
+    userAgentApplication.acquireTokenSilent(graphApiScopes)
+      .then(function (token) {
+        callWebApiWithToken(graphApiEndpoint, token, graphCallResponseElement, $("#accessToken"));
+      }, function (error) {
+        if (error) {
+          userAgentApplication.acquireTokenRedirect(graphApiScopes);
+        }
+      });
+  }
+}
+
+function loginCallback(errorDesc, token, error, tokenType) {
+  if (errorDesc) {
+    showError(msal.authority, error, errorDesc);
+  } else {
+    callGraphApi();
+  }
+}
+
+function showError(endpoint, error, errorDesc) {
+  var formattedError = JSON.stringify(error, null, 4);
+  if (formattedError.length < 3) {
+    formattedError = error;
+  }
+  $("#errorMessage").innerHTML = "An error has occurred:<br/>Endpoint: " + endpoint + "<br/>Error: " + formattedError + "<br/>" + errorDesc;
+  console.error(error);
+}
+
+function callWebApiWithToken(endpoint, token, responseElement, showTokenElement) {
+  var headers = new Headers();
+  var bearer = "Bearer: " + token;
+  headers.append("Authorization", bearer);
+  var options = {
+    method: "GET",
+    headers: headers
+  };
+
+  fetch(endpoint, options)
+    .then(function (response) {
+      var contentType = response.headers.get("content-type");
+      if (response.status === 200 && contentType && contentType.indexOf("application/json") !== -1) {
+        response.json()
+          .then(function (data) {
+            console.log(data);
+            responseElement.innerHTML = JSON.stringify(data, null, 4);
+            if (showTokenElement) {
+              showTokenElement.parentElement.classList.remove("hidden");
+              showTokenElement.innerHTML = token;
+            }
+          })
+          .catch(function (error) {
+            showError(endpoint, error);
+          });
+      } else {
+        response.json()
+          .then(function (data) {
+            showError(endpoint, data);
+          })
+          .catch(function (error) {
+            showError(endpoint, error);
+          });
+      }
+    })
+    .catch(function (error) {
+      showError(endpoint, error);
+    });
+}
+
+function signOut() {
+  userAgentApplication.logout();
+}
