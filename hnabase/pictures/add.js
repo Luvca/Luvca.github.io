47898847f37a058d90a281a7176d3e010720de1f
Luvca <committer@luvca.local>
2020-02-11 18:24:31 +0900

    update

--- hnabase/pictures/add.js
+++ hnabase/pictures/add.js
@@ -12,7 +12,7 @@
           event.preventDefault();
           event.stopPropagation();
         } else {
-          console.log($(".autocomplete-select"));
+          alert($(".autocomplete-select"));
         }
         form.classList.add('was-validated');
       }, false);
