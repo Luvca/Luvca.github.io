4beedd0a60df8958324ce052f6a4ce5d2e027c1b
Luvca <committer@luvca.local>
2020-02-11 17:17:49 +0900

    update

--- hnabase/pictures/add.js
+++ hnabase/pictures/add.js
@@ -1,25 +1,12 @@
 $(function () {
   'use strict';
 
-  $(document).ready(function () { 
-  $('input, select').on('change', function(event) {
-    var $element = $(event.target),
-      $container = $element.closest('.example');
-
-    if (!$element.data('tagsinput'))
-      return;
-
-    var val = $element.val();
-    if (val === null)
-      val = "null";
-    $('code', $('pre.val', $container)).html( ($.isArray(val) ? JSON.stringify(val) : "\"" + val.replace('"', '\\"') + "\"") );
-    $('code', $('pre.items', $container)).html(JSON.stringify($element.tagsinput('items')));
-  }).trigger('change');
-
-  $('#form').validator().on('submit', function (e) {
-    if (e.isDefaultPrevented()) {
-    } else {
-        alert('all OK !');
-    }
+  $(document).ready(function () {
+    $('#form').validator().on('submit', function (e) {
+      if (e.isDefaultPrevented()) {
+      } else {
+          alert('all OK !');
+      }
+    });
   });
 });
