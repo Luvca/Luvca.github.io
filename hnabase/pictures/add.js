c85b8d0eb84779c9bb7b6fe569e98bcfac1bff8a
Luvca <committer@luvca.local>
2020-02-13 23:12:22 +0900

    i want console

--- hnabase/pictures/add.js
+++ hnabase/pictures/add.js
@@ -37,5 +37,6 @@ db.collection("tags").get().then(function(querySnapshot) {
     querySnapshot.forEach(function(doc) {
         // doc.data() is never undefined for query doc snapshots
         alert(doc.id);
+    });
 });
 })();
