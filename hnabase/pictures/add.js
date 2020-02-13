6ec7ed9669d54bab1f1dfe27022130a5fd471b68
Luvca <committer@luvca.local>
2020-02-13 22:37:56 +0900

    alert

--- hnabase/pictures/add.js
+++ hnabase/pictures/add.js
@@ -29,6 +29,7 @@ var tags;
   //$('#debug').val(JSON.stringify(hnabase.dbtags()));
   //$('#debug').val(hnabase.debugText);
   hnabase.dbtags.get().then(function (docs) {
+    alert('well');
     $('#debug').val(JSON.stringify(docs));
   });
 })();
