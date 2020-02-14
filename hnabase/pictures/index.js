3956d0c84856f908bf2899d66e200146d84ea882
Luvca <committer@luvca.local>
2020-02-14 21:51:04 +0900

    update

--- hnabase/pictures/index.js
+++ hnabase/pictures/index.js
@@ -3,11 +3,12 @@
 var app = app || {};
 
 (function (app) {
-  var cardTemplate = ({ title, url, comment }) => `
+  var cardTemplate = ({ title, url, comment, tags }) => `
     <div class="card box-shadow">
       <img class="lazy card-img-top" data-original="${url}">
       <div class="card-body">
         <p class="card-text">${title}</p>
+        <p class="card-text">${tags}</p>
         <p class="card-text"><small class="text-muted">${comment}</small></p>
         <div class="d-flex justify-content-between align-items-center">
           <div class="btn-group">
@@ -20,21 +21,28 @@ var app = app || {};
     </div>
   `;
 
+  var tagsTemplate = ({ tag }) => `<span class="badge badge-danger">${tag}<span>`;
+
   var pictures = [];
   hnadata.db.collection("pictures").get().then(function (docs) {
     docs.forEach(function (doc) {
-      pictures.push(cardTemplate({ title: doc.data().title, url: doc.data().url, comment: doc.data().comment }));
+      pictures.push(cardTemplate({
+        title: doc.data().title,
+        url: doc.data().url,
+        comment: doc.data().comment,
+        tags: doc.data().tags.map(tagsTemplate).join('')
+      }));
       //pictures.push({ title: doc.data().title, url: doc.data().url, comment: doc.data().comment });
       //doc.data() is never undefined for query doc snapshots
     });
   }).then(function () {
     //$('#pictures').append(pictures.map(cardTemplate).join(''));
     $('#pictures').append(pictures.join(""));
-    $(function($) {
+    //$(function($) {
       $('img.lazy').lazyload({
         effect: 'fadeIn',
         effectspeed: 1000
       });
-    });
+    //});
   });
 }(app));
