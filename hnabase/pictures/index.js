e8530ac8a2c43d96c58e9f86f471dd51c42e7da0
Luvca <committer@luvca.local>
2020-02-14 23:55:07 +0900

    update

--- hnabase/pictures/index.js
+++ hnabase/pictures/index.js
@@ -21,7 +21,8 @@ var app = app || {};
 </div>
   `;
 
-  var tagsTemplate = (tag) => `<span class="badge badge-danger hna-tag">${tag}</span>`;
+  var tagsTemplate = (tag) => `<span class="badge badge-danger hna-tag">${tag}</span>
+  `;
 
   var pictures = [];
   hnadata.db.collection("pictures").get().then(function (docs) {
