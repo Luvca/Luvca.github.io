3c0348cd1a2aac62f3afe0dbf5efd57b9b475fcb
Luvca <committer@luvca.local>
2020-02-19 23:49:21 +0900

    order by

--- hnaapp/pictures/index.js
+++ hnaapp/pictures/index.js
@@ -25,7 +25,7 @@ var app = app || {};
   var tagsTemplate = (tag) => `<span class="badge badge-danger hna-tag">${tag}</span> `;
 
   var pictures = [];
-  hnadata.db.collection('pictures').get().orderBy('createTimestamp', 'desc').then(function (docs) {
+  hnadata.db.collection('pictures').orderBy('createTimestamp', 'desc').get().then(function (docs) {
     docs.forEach(function (doc) {
       pictures.push(cardTemplate({
         id: doc.id,
