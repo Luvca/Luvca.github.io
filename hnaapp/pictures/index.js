ff117ea2232657a7c16893e1fafe9c9a0767018b
Luvca <committer@luvca.local>
2020-02-21 23:11:36 +0900

    find

--- hnaapp/pictures/index.js
+++ hnaapp/pictures/index.js
@@ -7,13 +7,13 @@ var app = app || {};
     <div id="${picture.id}" class="card box-shadow">
       <img class="lazy card-img-top picture-url" data-original="${picture.url}">
       <div class="card-body">
-        <p class="card-text">${picture.title}</p>
+        <p class="card-text picture-title">${picture.title}</p>
         ${picture.tags}
         <p class="card-text"><small class="text-muted">${picture.comment}</small></p>
         <div class="d-flex justify-content-between align-items-center">
           <div class="btn-group">
             <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-            <button type="button" class="btn btn-sm btn-outline-secondary open-edit-dialog" data-toggle="modal" data-target="#editDialog" data-id="${picture.id}" data-url="${picture.url}" data-title="${picture.title}">Edit</button>
+            <button type="button" class="btn btn-sm btn-outline-secondary open-edit-dialog" data-toggle="modal" data-target="#editDialog" data-id="${picture.id}">Edit</button>
             <button type="button" class="btn btn-sm btn-outline-secondary" onclick="app.delete('${picture.id}');">Delete</button>
           </div>
         </div>
@@ -74,7 +74,8 @@ $(function() {
     var card = $(`#${id}`);
     //var url = button.data('url');
     var url = card.find('.picture-url').attr('src');
-    var title = button.data('title');
+    //var title = button.data('title');
+    var title = card.find('.picture-title').text();
     //alert(title);
     var dialog = $(this);
     dialog.find('.picture-id').val(id);
