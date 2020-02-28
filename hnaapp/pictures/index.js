31ff26bac07b9c8d39338a098368d3e9edb18cb9
Luvca <committer@luvca.local>
2020-02-28 20:35:51 +0900

    refs

--- hnaapp/pictures/index.js
+++ hnaapp/pictures/index.js
@@ -16,6 +16,7 @@ var app = app || {};
       <a class="picture-url" href="${picture.url}"><img class="lazy card-img-top" data-original="${picture.url}"></a>
       <div class="card-body">
         <p class="card-text picture-title">${picture.title}</p>
+        <p class="card-text womanref">${picture.womanRefs}</p>
         ${picture.women}
         ${picture.tags}
         <p class="card-text"><small class="text-muted">${picture.comment}</small></p>
